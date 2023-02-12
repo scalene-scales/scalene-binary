94b6386f2c877ff670b517c498178128aa9eeab1
Scalene Scales <scalene-scales@users.noreply.github.com>
2023-02-11 22:52:58 -0800

    Add missing exports.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export * as Constants from "./constants";
 export { default as decodeBase64StringAsDoubleArray } from "./decodeBase64StringAsDoubleArray";
+export { default as decodeBase64StringAsUint32Array } from "./decodeBase64StringAsUint32Array";
 export { default as encodeDoubleArrayAsBase64String } from "./encodeDoubleArrayAsBase64String";
+export { default as encodeUint32ArrayAsBase64String } from "./encodeUint32ArrayAsBase64String";
