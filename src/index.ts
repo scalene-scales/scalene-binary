28f6f37319a1405e39f0eb3af915d437f74bfbfe
Scalene Scales <scalene-scales@users.noreply.github.com>
2023-02-13 11:58:10 -0800

    Fix types try 2.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,7 @@
+import type * as TScaleneBinaryTypes from "./lib";
 import * as ScaleneBinary from "./lib";
 
-export type {
-  TBase64String,
-  TBase64String_Bits128,
-  TBase64String_Bits32,
-  TBase64String_Bits64,
-  TBase64String_Bits96,
-  TBase64String_BitsMod32,
-  TBase64String_BitsMod64,
-  THexString,
-  THexString_Length32,
-  THexString_LengthMod16,
-  THexString_LengthMod8,
-} from "./types";
-export { ScaleneBinary as ScaleneBinary };
+export type { TScaleneBinaryTypes };
+export { ScaleneBinary };
 
 export default ScaleneBinary;
